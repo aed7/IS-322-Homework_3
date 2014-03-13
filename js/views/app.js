f1d53ea8b93a7e994c61619ce19fa4b3d41fef9d
Alex E Davila unknown student <aed7@afsconnect1.njit.edu>
2014-03-13 16:40:34 -0400

    fixed bug with date picker

--- js/views/app.js
+++ js/views/app.js
@@ -13,7 +13,7 @@ define([
 	// Our overall **AppView** is the top-level piece of UI.
 	var AppView = Backbone.View.extend({
 
-		// Instead of generating a new element, bind to the existing skeleton of
+		// Instead of generating a new element, bind to the existing skeleton of 
 		// the App already present in the HTML.
 		el: '#todoapp',
 
